d941829517da79b6b1b6c95edfe9821ecfdb3ce9
regularskylark <mail@rx.id.au>
2019-11-30 19:35:18 +1030

    Peer-graded Assignment: Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## These functions calculate and cache the inverse of an input matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(mean) m <<- mean
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the matrix returned by makeCacheMatrix. Caches first inverse calculated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+}
+
+## Tests the functionality of makeCacheMatrix and cacheSolve given an invertable square matrix.
+
+test <- function(){
+        mat1 <-  matrix(c(2, -3, 4, -7), nrow=2, ncol=2, byrow = TRUE)
+    
+        m <- makeCacheMatrix(mat1)
+        print(m$get())
+    
+        print("Inverse")
+        inv1 <- cacheSolve(m)
+        print(inv1)
+    
+        print("Cached Inverse")
+        inv2 <- cacheSolve(m) # raises cached warning
+        print(inv2)
+    
+        m2 <- makeCacheMatrix(inv2)
+    
+        print("Inverse Inverse")
+        inv3 <- cacheSolve(m2)
+        print(inv3) #note this is the original matrix
 }
